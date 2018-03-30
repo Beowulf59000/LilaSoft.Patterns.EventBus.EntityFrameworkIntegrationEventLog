ff3630e1eb44fb0f3572b0b6d371e9f2cd33ad60
Johnny Behague <johnny.behague@live.fr>
2018-03-30 13:06:40 +0200

    avancement

---
+++ EntityFrameworkIntegrationEventLog/EntityFrameworkIntegrationEventLog/EventStateEnum.cs
@@ -0,0 +1,9 @@
+﻿namespace LilaSoft.Patterns.EventBus.EntityFrameworkIntegrationEventLog
+{
+    public enum EventStateEnum
+    {
+        NotPublished = 0,
+        Published = 1,
+        PublishedFailed = 2
+    }
+}
