b92f77e10662d662cefe270794e5a2ed878ded1f
Johnny Behague <johnny.behague@live.fr>
2018-03-30 13:06:40 +0200

    avancement

---
+++ EntityFrameworkIntegrationEventLog/EntityFrameworkIntegrationEventLog/Services/IIntegrationEventLogService.cs
@@ -0,0 +1,11 @@
+﻿namespace LilaSoft.Patterns.EventBus.EntityFrameworkIntegrationEventLog.Services
+{
+    using System.Threading.Tasks;
+    using System.Data.Common;
+
+    public interface IIntegrationEventLogService
+    {
+        Task SaveEventAsync(IntegrationEvent @event, DbTransaction transaction);
+        Task MarkEventAsPublishedAsync(IntegrationEvent @event);
+    }
+}
